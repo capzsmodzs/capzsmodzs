73aaf9ab6c49d4cedce49c62f15a16410da604ee
capzsmodzs <capzsmodzs@gmail.com>
2025-02-26 04:41:16 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -281,6 +281,32 @@ function pasang_domain() {
     fi
 }
 
+    # VERISIKASI IZIN
+    function verifikasi_izin() {
+            ipsaya=$(curl -sS ipv4.icanhazip.com)
+    data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+    date_list=$(date +"%Y-%m-%d" -d "$data_server")
+    data_ip="https://raw.githubusercontent.com/capzsmodzs/capzsmodzs/main/register"
+    checking_sc() {
+        useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
+        if [[ $date_list < $useexp ]]; then
+            echo -ne
+        else
+            echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+            echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+            echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+            echo -e ""
+            echo -e "            ${RED}PERMISSION DENIED !${NC}"
+            echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+            echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
+            echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+            exit 0
+        fi
+    }
+    checking_sc
+    clear
+    }
+
 clear
 #GANTI PASSWORD DEFAULT
 restart_system() {
@@ -930,6 +956,7 @@ function instal() {
     base_package
     make_folder_xray
     pasang_domain
+    verifikasi_izin
     password_default
     pasang_ssl
     install_xray
